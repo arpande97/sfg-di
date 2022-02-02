e50c654bdd12650a0ce3293f24964f4601db28a8
Archit Pande <architpande@Archits-MacBook-Pro.local>
2022-02-02 14:01:23 +0530

    DI using spring

--- src/test/java/com/example/sfgdi/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/com/example/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.example.sfgdi.controllers;
 
-import com.example.sfgdi.services.GreetingServiceImpl;
+import com.example.sfgdi.services.ConstructorInjectedGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ConstructorInjectedControllerTest {
 
@@ -13,7 +11,7 @@ class ConstructorInjectedControllerTest {
     @BeforeEach
     void setUp() {
 
-        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+        controller = new ConstructorInjectedController(new ConstructorInjectedGreetingService());
     }
 
     @Test

1c4c3d7589db4175fe514a58cc3f9edf30227cfe
arpande97 <architpande888@gmail.com>
2022-03-04 10:12:45 +0530

    after seeing the usages of bean scopes

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,12 +1,14 @@
 package com.example.sfgdi;
 
 import com.example.sfgdi.controllers.*;
+import com.example.sfgdi.services.PrototypeBean;
+import com.example.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan(basePackages = {"com.example.sfgdi", "com.example.pets"})
+@ComponentScan(basePackages = {"com.example.sfgdi", "com.example.pets"})
 //if you use java config, you don't need a component scan into other packages.
 @SpringBootApplication
 public class SfgDiApplication {
@@ -43,6 +45,17 @@ public class SfgDiApplication {
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("------Bean Scopes--------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
 	}
 
 }
